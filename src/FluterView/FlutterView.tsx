ac77ef339c392322f0e2ce58e38e370dff7eff30
lucascorreaa <lucas.lca14@gmail.com>
2024-06-04 10:29:35 -0300

    feat: added FlutterView in Root component

---
+++ src/FluterView/FlutterView.tsx
@@ -0,0 +1,109 @@
+import React, { useEffect, useRef, memo } from 'react';
+
+// Declaração da variável global _flutter
+declare var _flutter: any;
+
+const divStyle: React.CSSProperties = {
+  height: '80%',
+  width: '80%',
+  marginTop: '200px',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center', // Centraliza verticalmente
+};
+
+interface FlutterViewProps {
+  assetBase?: string;
+  src?: string;
+  onClicksChange?: (clicks: number) => void;
+  onScreenChange?: (screen: string) => void;
+  onTextChange?: (text: string) => void;
+
+  text: string;
+  screen: string;
+  clicks: number;
+}
+
+export const FlutterView: React.FC<FlutterViewProps> = memo(({
+  assetBase = '',
+  src = 'main.dart.js',
+  onClicksChange,
+  onScreenChange,
+  onTextChange,
+  text,
+  screen,
+  clicks,
+}) => {
+  const flutterState = useRef<any>(null);
+  const ref = useRef<HTMLDivElement>(null);
+
+  const onFlutterAppLoaded = (state: any) => {
+    flutterState.current = state;
+    // listen to state changes
+    state.onClicksChanged(onClicksChange);
+    state.onTextChanged(onTextChange);
+    state.onScreenChanged(onScreenChange);
+    // set initial values
+    state.setText(text);
+    state.setScreen(screen);
+    state.setClicks(clicks);
+  };
+
+  useEffect(() => {
+    const target = ref.current;
+    let isRendered = true;
+    const initFlutterApp = async () => {
+      if (!isRendered) return;
+      const engineInitializer = await new Promise<any>((resolve) => {
+        console.log('setup Flutter engine initializer...');
+        _flutter.loader.loadEntrypoint({
+          entrypointUrl: src,
+          onEntrypointLoaded: resolve,
+        });
+      });
+      if (!isRendered) return;
+
+      console.log('initialize Flutter engine...');
+      const appRunner = await engineInitializer?.initializeEngine({
+        hostElement: target,
+        assetBase: assetBase,
+      });
+      if (!isRendered) return;
+
+      console.log('run Flutter engine...');
+      await appRunner?.runApp();
+    };
+    initFlutterApp();
+
+    const eventListener = (event: Event) => {
+      let state = (event as CustomEvent).detail;
+      onFlutterAppLoaded(state);
+    };
+
+    target?.addEventListener('flutter-initialized', eventListener, {
+      once: true,
+    });
+
+    return () => {
+      isRendered = false;
+      target?.removeEventListener('flutter-initialized', eventListener);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    flutterState.current?.setText(text);
+  }, [text]);
+  useEffect(() => {
+    flutterState.current?.setScreen(screen);
+  }, [screen]);
+  useEffect(() => {
+    flutterState.current?.setClicks(clicks);
+  }, [clicks]);
+
+  return (
+    <div ref={ref} style={divStyle}>
+      <div style={{ textAlign: 'center' }}>Carregando...</div>
+    </div>
+  );
+});
--- src/root.component.tsx
+++ src/root.component.tsx
@@ -1,3 +1,25 @@
-export default function Root(props) {
-  return <section>{props.name} is mounted!</section>;
+import React, { useState } from 'react';
+import { FlutterView } from './FluterView/FlutterView';
+
+export default function Root() {
+  const [classNames, setClassNames] = useState('');
+  const [screen, setScreen] = useState('counter');
+  const [clicks, setClicks] = useState(0);
+  const [text, setText] = useState('');
+
+  return (
+    <div className={classNames}>
+      <FlutterView
+        assetBase="https://conecta-microfronts-qas.telemedicinaeinstein.com.br/flutter-main-web/"
+        src="https://conecta-microfronts-qas.telemedicinaeinstein.com.br/flutter-main-web/main.dart.js"
+        onClicksChange={setClicks}
+        onScreenChange={setScreen}
+        onTextChange={setText}
+        text={text}
+        clicks={clicks}
+        screen={screen}
+      />
+    </div>
+  );
 }
+
