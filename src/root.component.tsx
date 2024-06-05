0127581c1a0b808a24e1e6af0482944cc3a65cd3
lucascorreaa <lucas.lca14@gmail.com>
2024-06-04 21:18:09 -0300

    refactor: clean project

--- src/root.component.tsx
+++ src/root.component.tsx
@@ -1,23 +1,3 @@
-import React from 'react'
-import { FlutterView } from './FluterView/FlutterView'
-
-export default function Root() {
-  const [screen, setScreen] = React.useState('counter')
-  const [clicks, setClicks] = React.useState(0)
-  const [text, setText] = React.useState('')
-
-  return (
-      <div style={{width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-          <FlutterView
-            assetBase="https://conecta-microfronts-qas.telemedicinaeinstein.com.br/flutter-main-web/"
-            src="https://conecta-microfronts-qas.telemedicinaeinstein.com.br/flutter-main-web/main.dart.js"
-            onClicksChange={setClicks}
-            onScreenChange={setScreen}
-            onTextChange={setText}
-            text={text}
-            clicks={clicks}
-            screen={screen}
-          />
-        </div>
-  )
+export default function Root(props) {
+  return <section>{props.name} is mounted!</section>;
 }
